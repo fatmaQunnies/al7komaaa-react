2ecf483c4e9143dc32fa03e5205cb66b9d3cde12
fatmaQunnies <fatima.n.qunnies@gmail.com>
2024-05-06 23:49:40 +0300

    git 10 post and readMore

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [user, setUser] = useState([{username:'',image:''}]);
-  const [token, setToken] = useState('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXRtYTIiLCJpYXQiOjE3MTQ5Mzc2NDcsImV4cCI6MTcxNTAyNDA0N30.WStLsdVsJakWTQNjbik0tJUh_UduB2hPKvH6oln37v0');
-
+  const [token, setToken] = useState('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXRtYTIiLCJpYXQiOjE3MTUwMjQxOTgsImV4cCI6MTcxNTExMDU5OH0.0VvymbrhFE56iPGU6HsuJ1Z_ij8o8_1Bb4ZbBVSYVrQ');
   const [postContent, setPostContent] = useState([]);
+  const [readMore, setReadMore] = useState();
 
           const [userName,setUserName]=useState('');
           const [userImage,setUserImage]=useState('');
@@ -47,7 +47,7 @@ function App() {
 
   useEffect(() => {
     console.log("USEEFFECT == posts" ) ;
-    fetch('http://localhost:8080/post/posts', {
+    fetch('http://localhost:8080/post/posts/random', {
       headers: {
           'Authorization': 'Bearer ' + token 
       }
@@ -55,13 +55,37 @@ function App() {
     .then(response => response.json())
     .then(data => {
       setPostContent(data._embedded.posts);
+      setReadMore(data);
       console.log(data._embedded.posts +"pooooood");
     })
     .catch(error => console.error('Error fetching data:', error));
   }, []);// لازم نغير الديبيندنسي 
 
-
-
+  const handReadMore = async () => {
+    try {
+      const response = await fetch(readMore._links["read more"].href, {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        }
+      });
+      const responseData = await response.json();
+      console.log(responseData._embedded.posts, "READ MORE");
+      if (response.ok) {
+        const newPosts = responseData._embedded.posts.filter(newPost => {
+          return !postContent.some(oldPost => oldPost.id === newPost.id);
+        });
+        setPostContent([...postContent, ...newPosts]);
+        console.log("READ MORE");
+      } else {
+        console.error('Error:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
   
 
   return (
@@ -78,10 +102,12 @@ function App() {
     info={post}
     userName={userName}
     userImage={userImage}
+
   />
+
 ))}
-      
-    </div>
+   <a onClick={handReadMore}>Read More</a>
+ </div>
   );
 }
 
