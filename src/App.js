1bd724a51eefb3f47f57e04545976e834533b4d6
fatmaQunnies <fatima.n.qunnies@gmail.com>
2024-05-13 19:03:11 +0300

    rou

--- src/App.js
+++ src/App.js
@@ -1,119 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
-import Post from './Post.jsx';
-import LeftList from './LeftList.jsx';
-import RightList from './RightList.jsx';
-import { Routes, Route, Link, BrowserRouter as Router } from 'react-router-dom'; 
-import Notfound from './Notfound.jsx';
-import Navbar from './Navbar.jsx';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Post from "./Post.jsx";
+import LeftList from "./LeftList.jsx";
+import Notfound from "./Notfound.jsx";
+import Navbar from "./Navbar.jsx";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  const [user, setUser] = useState([{username:'',image:''}]);
-  const [token, setToken] = useState('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXRtYTIiLCJpYXQiOjE3MTU1MjcxMTgsImV4cCI6MTcxNTYxMzUxOH0.CxALTknpaHj8LbAL916NbOC7yULU1UVrLAuKW8Bt9QM');
+  const [user, setUser] = useState([{ username: "", image: "" }]);
+  const [token, setToken] = useState(
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXRtYTIiLCJpYXQiOjE3MTU2MTUyOTIsImV4cCI6MTcxNTcwMTY5Mn0.RpXcb-X2CDTaYN7A5WKHw_pzntrosH41pDB8_DHjLlY"
+  );
   const [postContent, setPostContent] = useState([]);
   const [readMore, setReadMore] = useState();
-  var count=0;
-  const [userName,setUserName]=useState('');//
-  const [userImage,setUserImage]=useState('');//////
-  const [userInfo,setUserinfo]=useState([]);
+  const [userName, setUserName] = useState(""); //
+  const [userImage, setUserImage] = useState(""); ////
+  const [userInfo, setUserinfo] = useState([]);
 
   useEffect(() => {
-    console.log("USEEFFECT == " ) ;
-    fetch('http://localhost:8080/myUserName', {
+    console.log("USEEFFECT == ");
+    fetch("http://localhost:8080/myUserName", {
       headers: {
-        'Authorization': 'Bearer ' + token
-      }
+        Authorization: "Bearer " + token,
+      },
     })
-    .then(response => response.json())
-    .then(data => {
-      setUserinfo(data);
-    })
-    .catch(error => console.error('Error fetching data:', error));
+      .then((response) => response.json())
+      .then((data) => {
+        setUserinfo(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   useEffect(() => {
-    console.log("USEEFFECT == " ) ;
-    fetch('http://localhost:8080/getImage', {
+    console.log("USEEFFECT == ");
+    fetch("http://localhost:8080/getImage", {
       headers: {
-        'Authorization': 'Bearer ' + token
-      }
+        Authorization: "Bearer " + token,
+      },
     })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      setUserImage(data);
-    })
-    .catch(error => console.error('Error fetching data:', error));
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        setUserImage(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   useEffect(() => {
-    console.log("USEEFFECT == posts" ) ;
-    fetch('http://localhost:8080/post/posts/random', {
+    console.log("USEEFFECT == posts");
+    fetch("http://localhost:8080/post/posts/random", {
       headers: {
-        'Authorization': 'Bearer ' + token 
-      }
+        Authorization: "Bearer " + token,
+      },
     })
-    .then(response => response.json())
-    .then(data => {
-      setPostContent(data._embedded.posts);
-      setReadMore(data);
-      console.log(data._embedded.posts +"pooooood");
-    })
-    .catch(error => console.error('Error fetching data:', error));
+      .then((response) => response.json())
+      .then((data) => {
+        setPostContent(data._embedded.posts);
+        setReadMore(data);
+        console.log(data._embedded.posts + "pooooood");
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   const handReadMore = async () => {
     try {
       const response = await fetch(readMore._links["read more"].href, {
-        method: 'GET',
+        method: "GET",
         headers: {
-          Authorization: 'Bearer ' + token,
-          'Content-Type': 'application/json'
-        }
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
       });
       const responseData = await response.json();
       console.log(responseData._embedded.posts, "READ MORE");
       if (response.ok) {
-        const newPosts = responseData._embedded.posts.filter(newPost => {
-          return !postContent.some(oldPost => oldPost.id === newPost.id);
+        const newPosts = responseData._embedded.posts.filter((newPost) => {
+          return !postContent.some((oldPost) => oldPost.id === newPost.id);
         });
         setPostContent([...postContent, ...newPosts]);
         console.log("READ MORE");
       } else {
-        console.error('Error:', response.statusText);
+        console.error("Error:", response.statusText);
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
-  const [userfriend,setUserFriend]=useState([]);
+  const [userfriend, setUserFriend] = useState([]);
   useEffect(() => {
-    console.log("USEEFFECT == " ) ;
-    fetch('http://localhost:8080/friendSuggestion', {
+    console.log("USEEFFECT == ");
+    fetch("http://localhost:8080/friendSuggestion", {
       headers: {
-        'Authorization': 'Bearer ' + token
-      }
+        Authorization: "Bearer " + token,
+      },
     })
-    .then(response => response.json())
-    .then(data => {
-      setUserFriend(data);
-    })
-    .catch(error => console.error('Error fetching data:', error));
+      .then((response) => response.json())
+      .then((data) => {
+        setUserFriend(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
-    <div className="App">
-      <LeftList key={userInfo.id} data={userInfo} token={token}></LeftList>
-      <nav className="navbar">
-        <Navbar />
-      </nav>
-      <Router>
+    <Router>
+      <div className="App">
+        <LeftList key={userInfo.id} data={userInfo} token={token}></LeftList>
+        <nav className="navbar">
+          <Navbar />
+        </nav>
         <Routes>
-          <Route path="/feed" element={
-            postContent.map(post => (
-              <Post className="post"
+          <Route
+            path="/feed"
+            element={postContent.map((post) => (
+              <Post
+                className="post"
                 key={post.id}
                 id={post.id}
                 token={token}
@@ -121,18 +122,18 @@ function App() {
                 userName={userName}
                 userImage={userImage}
               />
-            ))
-          } />
-          <Route path="Friends" element={<Notfound />} />
-          <Route path="profile" element={<Notfound />} />
-          <Route path="Notification" element={<Notfound />} />
-          <Route path="Reel" element={<Notfound />} />
-          <Route path="Setting" element={<Notfound />} />
-          <Route path="Messages" element={<Notfound />} />
-          <Route path="Likes" element={<Notfound />} />
+            ))}
+          />
+          <Route path="/Friends" element={<Notfound />} />
+          <Route path="/profile" element={<Notfound />} />
+          <Route path="/Notification" element={<Notfound />} />
+          <Route path="/Reel" element={<Notfound />} />
+          <Route path="/Setting" element={<Notfound />} />
+          <Route path="/Messages" element={<Notfound />} />
+          <Route path="/Likes" element={<Notfound />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,39 +1,37 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Feed</Link>
-          </li>
-          <li>
-            <Link to="/Post">Friends</Link>
-          </li>
-          <li>
-            <Link to="/Post">profile</Link>
-          </li>
-          <li>
-            <Link to="/Post">Notification</Link>
-          </li>
-          <li>
-            <Link to="/Post">Reel</Link>
-          </li>
-          <li>
-            <Link to="/Post">Setting</Link>
-          </li>
-          <li>
-            <Link to="/Post">Messages</Link>
-          </li>
-          <li>
-            <Link to="/Post">Likes</Link>
-          </li>
-        </ul>
-      </nav>
-
-    </>
-  )
+    <nav>
+      <ul>
+        <li>
+          <Link to="/feed">Feed</Link>
+        </li>
+        <li>
+          <Link to="/Friends">Friends</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/Notification">Notification</Link>
+        </li>
+        <li>
+          <Link to="/Reel">Reel</Link>
+        </li>
+        <li>
+          <Link to="/Setting">Setting</Link>
+        </li>
+        <li>
+          <Link to="/Messages">Messages</Link>
+        </li>
+        <li>
+          <Link to="/Likes">Likes</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
